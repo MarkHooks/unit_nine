da0c289eb63d0b5cc21eef1e0cc2c7f242f6d75e
Mark <mark@iMac2.local>
2018-11-16 13:52:35 -0500

    created deck class

---
+++ card.py
@@ -0,0 +1,22 @@
+class Card:
+
+    def __init__(self, rank, suit):
+
+        self.rank = rank
+        self.suit = suit
+
+    def compared_to(self, other_card):
+        """
+        this is to compare cards
+        :param other_card: the other persons card
+        :return: this is to return a card
+        """
+        if self.rank > other_card.rank:
+            return self
+        elif other_card.rank > self.rank:
+            return other_card
+        else:
+            if self.suit > other_card.suit:
+                return self
+            else:
+                return other_card
