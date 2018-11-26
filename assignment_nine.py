6a0158a7add57cb2c93d4099d79043747448d1bf
Mark <mark@iMac2.local>
2018-11-26 15:11:44 -0500

    got it to print cards and compare

--- assignment_nine.py
+++ assignment_nine.py
@@ -16,8 +16,34 @@ def deal(deckocards):
         computer_cards.append(deckocards.draw_a_card())
     return user_card, computer_cards
 
+
+def show_cards(user_card, computer_cards):
+    print("user has a ", user_card)
+    print("the computer has a ", computer_cards)
+
+
+def compare(user_card, computer_cards):
+    higher = user_card.compared_to(computer_cards)
+    user_total = 0
+    computer_total = 0
+    if user_card == higher:
+        user_total = + 1
+        print("user gets a point")
+    else:
+        computer_total = + 1
+        print("computer gets a point")
+    return computer_total, user_total
+
 def main():
+
     deckocards = create_deck()
-    deal(deckocards)
-    draw_a_card()
-    compare()
+    user_card, computer_cards = deal(deckocards)
+    for x in range(len(user_card)):
+        show_cards(user_card[x], computer_cards[x])
+        computer_total, user_total = (user_card[x], computer_cards[x])
+    if user_total > computer_total:
+        print("you win")
+    else:
+        print("computer wins")
+
+main()
